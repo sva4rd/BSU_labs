749e2a9ac3bd2d1bfbe44293f04be8c12869c12e
sva4rd <egor71175@gmail.com>
2023-04-12 09:39:22 +0300

    ref creator

--- osi/lab_1/src/creator.cpp
+++ osi/lab_1/src/creator.cpp
@@ -2,32 +2,37 @@
 #include <fstream>
 #include "../include/employee.h"
 
+template < typename T >
+void enterData(std::string attributeName, T &attribute)
+{
+    std::cout << "\t" + attributeName + ": ";
+    std::cin >> attribute;
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         std::cerr << "Error: must be 3 args";
         return 1;
     }
 
-    std::ofstream writeFile(argv[1], std::ios::binary);
-    if (!writeFile) {
+    std::ofstream binFile(argv[1], std::ios::binary);
+    if (!binFile) {
         std::cerr << "Error: Could not open bin file";
         return 1;
     }
 
-    int num_records = std::atoi(argv[2]);
-    employee record;
-
-    for (int i = 0; i < num_records; i++) {
-        std::cout << "Enter the details of employee " << i + 1 << ":" << '\n';
-        std::cout << "ID: ";
-        std::cin >> record.num;
-        std::cout << "Name: ";
-        std::cin >> record.name;
-        std::cout << "Hours: ";
-        std::cin >> record.hours;
-        writeFile.write(reinterpret_cast<char*>(&record), sizeof(record));
+    int amount = std::atoi(argv[2]);
+    employee rec;
+    std::cout << "Enter employees data:" << '\n';
+    for (int i = 0; i < amount; i++) 
+    {
+        enterData("ID", rec.num);
+        enterData("Name", rec.name);
+        enterData("Hours", rec.hours);
+        std::cout << '\n';
+        binFile.write((char*)(&rec), sizeof(rec));
     }
     
-    writeFile.close();
+    binFile.close();
     return 0;
 }
