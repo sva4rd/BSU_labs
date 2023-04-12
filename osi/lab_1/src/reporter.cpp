acc6bfe1382f7f1a8c8acea2a72c554ee3a763c9
sva4rd <egor71175@gmail.com>
2023-04-12 09:38:58 +0300

    ref reporter

--- osi/lab_1/src/reporter.cpp
+++ osi/lab_1/src/reporter.cpp
@@ -3,42 +3,48 @@
 #include <string>
 #include "../include/employee.h"
 
+void writeData(std::ifstream &binFile, std::ofstream &repFile, double salaryPerHour)
+{
+    employee rec;
+    repFile << "\nEmployee \tName\tHours\tSalary\n";
+    while (binFile.read((char*)(&rec), sizeof(rec)))
+    {
+        double salary = rec.hours * salaryPerHour;
+        repFile << rec.num 
+            << "\t\t" << rec.name 
+            << "\t" << rec.hours 
+            << "\t" << salary << "$\n";
+    }
+}
+
+template < typename T >
+void checkFile(T &file, std::string attributeName)
+{
+    if (!file)
+    {
+        std::cerr << "Error: opening " + attributeName+ " file";
+        exit(1);
+    }
+}
+
 int main(int argc, char* argv[])
 {
     if (argc != 4)
     {
-        std::cerr << "Error: Argument exception (must be equal 4)" << std::endl;
+        std::cerr << "Error: must be 4 args" << std::endl;
         return 1;
     }
 
     std::string binFileName = argv[1];
-    std::string repFileName = argv[2];
-    double salaryPerHour = std::stod(argv[3]);
-
     std::ifstream binFile(binFileName, std::ios::binary);
-    if (!binFile)
-    {
-        std::cerr << "Error opening binary file" << binFileName;
-        return 1;
-    }
+    checkFile(binFile, "bin");
 
+    std::string repFileName = argv[2];
     std::ofstream repFile(repFileName);
-    if (!repFile)
-    {
-        std::cerr << "Error opening report file" << repFileName;
-        return 1;
-    }
+    checkFile(repFile, "report");
 
-    repFile << "Report based on binary file" << binFileName;
-    repFile << "\nEmployee \tName\tHours\tSalary\n";
-
-    employee employee;
-    while (binFile.read((char*)&employee, sizeof(employee)))
-    {
-        double salary = employee.hours * salaryPerHour;
-        repFile << employee.num << "\t\t" << employee.name << "\t" << employee.hours << "\t" << salary << "$\n";
-    }
-
+    double salaryPerHour = std::stod(argv[3]);
+    writeData(binFile, repFile, salaryPerHour);
     binFile.close();
     repFile.close();
     return 0;
